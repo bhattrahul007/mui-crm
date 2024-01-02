bb7c250c4e1e48f295df69bc39ecddedda3a2569
Rahul Kumar Bhatt <bhattrahul270@gmail.com>
2024-01-02 12:02:55 +0530

    seperate parseRequest into func

--- src/middleware/api.ts
+++ src/middleware/api.ts
@@ -1,38 +1,36 @@
 /* eslint-disable */
-import { DB } from "./demo-db"
+// api for interacting with json-server
+import { DB } from "./demo-db";
 
-import { Entity } from '../types';
-import url from 'url';
-import querystring from 'querystring';
+import { Entity } from "../types";
+import url from "url";
+import querystring from "querystring";
 import { HttpMethod } from "../store/types";
 import { getSeachFilters } from "../utils/app-utils";
 
-const ds = Object.assign({}, DB)
-const EXPAND = "_expand"
+const ds = Object.assign({}, DB);
+const EXPAND = "_expand";
 
 function getModel(action: string) {
   if (action.includes("/")) {
-    return action.substring(0, action.indexOf("/"))
-  }
-  else {
+    return action.substring(0, action.indexOf("/"));
+  } else {
     return action;
   }
 }
 
 function getId(action: string): number {
   if (action.includes("/")) {
-    return parseInt(action.substring(action.indexOf("/") + 1))
-  }
-  else {
-    return 0
+    return parseInt(action.substring(action.indexOf("/") + 1));
+  } else {
+    return 0;
   }
 }
 
 function getExpand(qs: TODO) {
   if (EXPAND in qs) {
     return qs[EXPAND];
-  }
-  else return ''
+  } else return "";
 }
 
 function parseRequest(req: string) {
@@ -40,13 +38,13 @@ function parseRequest(req: string) {
   const parsedQs = querystring.parse(parsedUrl.query);
   const model = getModel(parsedUrl.pathname);
   const id = getId(parsedUrl.pathname);
-  const exp = getExpand(parsedQs)
-  const filters = getSeachFilters(parsedQs)
-  return { model, id, exp, filters }
+  const exp = getExpand(parsedQs);
+  const filters = getSeachFilters(parsedQs);
+  return { model, id, exp, filters };
 }
 
 export function getData(action: string): Promise<TODO> {
-  const { model, id, exp , filters} = parseRequest(action)
+  const { model, id, exp, filters } = parseRequest(action);
   return new Promise(function (resolve, _reject) {
     const expandModel = exp
       ? exp === "category"
@@ -54,10 +52,9 @@ export function getData(action: string): Promise<TODO> {
         : exp + "s"
       : exp;
 
-    
-      let result: TODO;
+    let result: TODO;
     let expand: string, expandId: number;
-    
+
     if (model in ds) {
       if (id && id > 0) {
         result =
@@ -70,7 +67,9 @@ export function getData(action: string): Promise<TODO> {
           expandId = result[expand + "Id"] as number;
           result[expand] =
             ds[expandModel][
-            ds[expandModel].findIndex((d: { id: number }) => d.id === expandId)
+              ds[expandModel].findIndex(
+                (d: { id: number }) => d.id === expandId
+              )
             ];
         }
       } else {
@@ -83,20 +82,23 @@ export function getData(action: string): Promise<TODO> {
             expandId = m[expand + "Id"] as number;
             m[expand] =
               ds[expandModel][
-              ds[expandModel].findIndex((d: { id: number }) => d.id === expandId)
+                ds[expandModel].findIndex(
+                  (d: { id: number }) => d.id === expandId
+                )
               ];
           }
           return m;
         });
       }
 
-      if (filters !== null && filters !== undefined
-        && Object.keys(filters).length > 0) {
-        result = result.filter(
-          row => Object.keys(filters).every(
-            prop => filters[prop](prop,row)
-          )
-        )
+      if (
+        filters !== null &&
+        filters !== undefined &&
+        Object.keys(filters).length > 0
+      ) {
+        result = result.filter((row) =>
+          Object.keys(filters).every((prop) => filters[prop](prop, row))
+        );
       }
     }
     setTimeout(resolve, 300, { data: result });
@@ -104,7 +106,7 @@ export function getData(action: string): Promise<TODO> {
 }
 
 export function postData(action: string, data: Entity): Promise<TODO> {
-  const { model } = parseRequest(action)
+  const { model } = parseRequest(action);
   return new Promise(function (resolve, _reject) {
     ds[model].push(data);
     setTimeout(resolve, 300, { data: data });
@@ -112,7 +114,7 @@ export function postData(action: string, data: Entity): Promise<TODO> {
 }
 
 export function putData(action: string, data: Entity): Promise<TODO> {
-  const { model, id } = parseRequest(action)
+  const { model, id } = parseRequest(action);
   return new Promise(function (resolve, _reject) {
     const idx = ds[model].findIndex((d: { id: number }) => d.id === id);
     ds[model][idx] = Object.assign({}, data);
@@ -121,16 +123,23 @@ export function putData(action: string, data: Entity): Promise<TODO> {
 }
 
 export function deleteData(action: string): Promise<TODO> {
-  const { model, id } = parseRequest(action)
+  const { model, id } = parseRequest(action);
   return new Promise(function (resolve, _reject) {
     if (id > 0) {
-      ds[model].splice(ds[model].findIndex((d: Entity) => d.id === id), 1);
+      ds[model].splice(
+        ds[model].findIndex((d: Entity) => d.id === id),
+        1
+      );
     }
     setTimeout(resolve, 300, { data: id });
   });
 }
 
-export function login(action: string, _method: HttpMethod, data: TODO): Promise<TODO> {
+export function login(
+  action: string,
+  _method: HttpMethod,
+  data: TODO
+): Promise<TODO> {
   return new Promise(function (resolve, _reject) {
     if (data.username === "admin@test.com" && data.password === "password") {
       const { accessToken: accessToken, user } = ds.token;
@@ -147,22 +156,24 @@ export function login(action: string, _method: HttpMethod, data: TODO): Promise<
   });
 }
 
-export function callApi(endpoint, method: HttpMethod, data?: TODO, filters?: TODO) {
+export function callApi(
+  endpoint,
+  method: HttpMethod,
+  data?: TODO,
+  filters?: TODO
+) {
   switch (method) {
     case HttpMethod.GET:
       return getData(endpoint);
     case HttpMethod.PUT:
       return putData(endpoint, data);
     case HttpMethod.POST:
-      return postData(endpoint, data)
+      return postData(endpoint, data);
     case HttpMethod.DELETE:
-      return deleteData(endpoint)
+      return deleteData(endpoint);
     default:
       return null;
-
   }
-
 }
 
-export const CALL_API = Symbol("Call API")
-
+export const CALL_API = Symbol("Call API");
